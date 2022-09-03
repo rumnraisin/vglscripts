cc4e1c4569981155739bb9438b788e9619680ba2
Denkocchi <56016391+Denkocchi@users.noreply.github.com>
2022-09-03 07:19:17 -0700

    Update m.js

--- m.js
+++ m.js
@@ -61,12 +61,10 @@ $( document ).ready(function() {
 	
 		//Match Schedule
 		$('#nav-collapsible ul:first-child').append("<li class='dropdown'><a class='dropdown-toggle' href='#' data-toggle='dropdown' aria-expanded='false'>Match Schedule<b class='caret'></b></a><ul class='dropdown-menu' id='matchSchedule'><li>Times in UTC</li></ul></li>");
-		for(var i = 1; i <= 24; i++){
+		for(var i = 1; i <= 12; i++){
 			if (i == 1){
-				$('#matchSchedule').append('<li id="day1"></li>');
-			} else if (i == 9) {
 				$('#matchSchedule').append('<li id="day2"></li>');
-			} else if (i == 17){
+			} else if (i == 7) {
 				$('#matchSchedule').append('<li id="day3"></li>');
 			}
 			$('#matchSchedule').append('<li id="match' + i + '"></li>');
