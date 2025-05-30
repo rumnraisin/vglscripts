e404530445096b6a08068595b2eef6b17bf2910d
Denkocchi <56016391+Denkocchi@users.noreply.github.com>
2025-05-30 07:55:12 -0700

    Update teamcolor_vgl.js
    forgot vgt

--- teamcolor_vgl.js
+++ teamcolor_vgl.js
@@ -277,7 +277,6 @@ var TeamLists = {
 		{id:"twg",			color:"#990000",icon:"/w/images/b/b2/Twg_icon.png"},
 		{id:"uma",			color:"#796BAA",icon:"/w/images/2/22/Uma_icon.png"},
 		{id:"utg",			color:"#67A4E0",icon:"/w/images/d/d1/Utg_icon.png"},
-		{id:"vgt",			color:"#BFBFBF",icon:"/w/images/0/0a/Vgt_icon.png"},
 		{id:"vn",			color:"#FFFFFF",icon:"/w/images/f/fb/Vn_icon.png"},
 		{id:"vrg",			color:"#34c7cf",icon:"/w/images/d/d6/Vrg_icon.png"},
 		{id:"wowg",			color:"#FFF468",icon:"/w/images/9/9d/Wowg_icon.png"},
